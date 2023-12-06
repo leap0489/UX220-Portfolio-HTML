1fc9a5e28fe61ebe5971f779f7785f4f65a48a05
leap0489 <eapx0489@mylaurier.ca>
2023-12-06 17:09:41 -0500

    added name custom element

--- customelement.js
+++ customelement.js
@@ -6,3 +6,10 @@ class FooterElement extends HTMLElement{
 
 customElements.define("x-footeryear",FooterElement);
 
+class NameElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = "Laynol Eap";
+    }
+}
+
+customElements.define("x-name",NameElement);
