c68f68e94c1f1692af0d15d3d1eb6e252606979d
leap0489 <eapx0489@mylaurier.ca>
2023-12-06 15:02:20 -0500

    added custom element and footer nav to index

---
+++ customelement.js
@@ -0,0 +1,7 @@
+class FooterElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = new Date().getFullYear();
+    }
+}
+
+customElements.define("x-footeryear",FooterElement);
