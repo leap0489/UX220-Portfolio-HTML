38e18170094060861b4b09987fb590697a1cd226
leap0489 <eapx0489@mylaurier.ca>
2023-12-06 15:17:42 -0500

    added stuff

--- customelement.js
+++ customelement.js
@@ -5,3 +5,11 @@ class FooterElement extends HTMLElement{
 }
 
 customElements.define("x-footeryear",FooterElement);
+
+class FooterElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = new Date().getFullYear();
+    }
+}
+
+customElements.define("x-footeryear",FooterElement);
